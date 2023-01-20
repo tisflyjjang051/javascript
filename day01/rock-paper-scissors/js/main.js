b5e213c4e2f98039a567b6f3c7726e61874be9f9
jang sung ho <tisflyjjang051@gmail.com>
2023-01-20 14:04:03 +0900

    append item

--- day01/rock-paper-scissors/js/main.js
+++ day01/rock-paper-scissors/js/main.js
@@ -1,6 +1,33 @@
 const comItems = document.querySelectorAll("#com ul li");
 const playerItems = document.querySelectorAll("#player ul li");
-let num = 0;
+const resultList = document.querySelector("#result ul");
+let num = 0; // 전역 변수 scope  let, const {}  var function() {}
+
+function appendWinItem() {
+  const li = document.createElement("li");
+  li.classList.add("win");
+  li.textContent = "W";
+  resultList.appendChild(li);
+}
+function appendDrawItem() {
+  const li = document.createElement("li");
+  li.classList.add("draw");
+  li.textContent = "D";
+  resultList.appendChild(li);
+}
+function appendLoseItem() {
+  const li = document.createElement("li");
+  li.classList.add("lose");
+  li.textContent = "L";
+  resultList.appendChild(li);
+}
+function appendItem(classTxt, txt) {
+  const li = document.createElement("li");
+  li.classList.add(classTxt);
+  li.textContent = txt;
+  resultList.appendChild(li);
+}
+
 function random() {
   for (let i = 0; i < 3; i++) {
     comItems[i].style.display = "none";
@@ -12,8 +39,17 @@ function random() {
 for (let i = 0; i < playerItems.length; i++) {
   playerItems[i].addEventListener("click", function () {
     clearInterval(idx);
-    console.log(i);
-    console.log(num);
+    //console.log(i, "====", num);
+    if (i === num) {
+      //비겼을때
+      appendItem("draw", "D");
+    } else if ((i === 0 && num === 2) || (i === 1 && num === 0) || (i === 2 && num === 1)) {
+      //이긴경우
+      appendItem("win", "W");
+    } else {
+      //졌을경우
+      appendItem("lose", "L");
+    }
   });
 }
 const idx = setInterval(random, 20);
