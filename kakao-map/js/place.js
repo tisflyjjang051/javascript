93801e87304ae8e53637628f6307bab45464a1dc
jang sung ho <tisflyjjang051@gmail.com>
2023-01-31 11:35:48 +0900

    custom overlay

--- kakao-map/js/place.js
+++ kakao-map/js/place.js
@@ -4,14 +4,21 @@ searchTxt.addEventListener("keyup", function (e) {
     searchPlace(searchTxt.value);
   }
 });
+
+const container = document.querySelector("#map");
+
 function searchPlace(searchTxt) {
-  const container = document.querySelector("#map");
+  container.innerHTML = "";
   const mapOption = {
     center: new kakao.maps.LatLng(37.66826, 126.9786567),
     level: 3,
   };
   const map = new kakao.maps.Map(container, mapOption);
-
+  //const infoWindow = new kakao.maps.InfoWindow({ zIndex: 99, removable: true });
+  const customOverlay = new kakao.maps.CustomOverlay({
+    map: map,
+    content: `<div class="contents-box"><h1>나는 커스텀 오버레이</h1></div>`,
+  });
   const place = new kakao.maps.services.Places();
   place.keywordSearch(searchTxt, function (result, status) {
     console.log(status);
@@ -22,6 +29,28 @@ function searchPlace(searchTxt) {
         const marker = new kakao.maps.Marker({
           map: map,
           position: new kakao.maps.LatLng(item.y, item.x),
+        });
+        /*
+        kakao.maps.event.addListener(marker, "click", function () {
+          //prettier-ignore
+          infoWindow.setContent(`<div>${item.place_name}</div>
+                                 <div>${item.phone}</div>
+                                 <div>${item.road_address_name}</div>
+                                 <div><a href="${item.place_url}" target="_blank">매장정보</a></div>`);
+          infoWindow.open(map, marker);
+        });
+        */
+        kakao.maps.event.addListener(marker, "click", function () {
+          customOverlay.setContent(
+            `<div class="contents-box">
+              <div class="title">${item.place_name}</div>
+              <div class="phone">${item.phone}</div>
+              <div class="address">${item.road_address_name}</div>
+              <div class="info"><a href="${item.place_url}" target="_blank">매장정보</a></div>
+            </div>`
+          );
+          customOverlay.setMap(map);
+          customOverlay.setPosition(marker.getPosition());
         });
         bounds.extend(new kakao.maps.LatLng(item.y, item.x));
       });
